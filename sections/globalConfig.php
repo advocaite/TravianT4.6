3afb4530cdcc5905a7e6814f23c5caec10de9521
Advocaite <mcrodneyau@gmail.com>
2024-06-21 20:37:19 +0800

    Create globalConfig.php

---
+++ sections/globalConfig.php
@@ -0,0 +1,30 @@
+<?php
+global $globalConfig;
+$globalConfig = [];
+$globalConfig['staticParameters'] = [];
+$globalConfig['staticParameters']['default_language'] = 'gr';
+$globalConfig['staticParameters']['default_timezone'] = 'Australia/Sydney';
+$globalConfig['staticParameters']['default_direction'] = 'LTR';
+$globalConfig['staticParameters']['default_dateFormat'] = 'y.m.d';
+$globalConfig['staticParameters']['default_timeFormat'] = 'H:i';
+$globalConfig['staticParameters']['indexUrl'] = 'https://www.example.com/';
+$globalConfig['staticParameters']['forumUrl'] = 'https://forum.example.com/';
+$globalConfig['staticParameters']['answersUrl'] = 'https://answers.travian.com/index.php';
+$globalConfig['staticParameters']['helpUrl'] = 'https://help.example.com/';
+$globalConfig['staticParameters']['adminEmail'] = '';
+$globalConfig['staticParameters']['session_timeout'] = 6*3600;
+$globalConfig['staticParameters']['default_payment_location'] = 2;
+$globalConfig['staticParameters']['global_css_class'] = 'travuser';
+$globalConfig['staticParameters']['gpacks'] = require("/travian/sectiond/gpack/gpack.php");
+$globalConfig['staticParameters']['recaptcha_public_key'] = '';
+$globalConfig['staticParameters']['recaptcha_private_key'] = '';
+$globalConfig['cachingServers'] = ['memcached' => [['127.0.0.1', 11211],],];
+$globalConfig['dataSources'] = [];
+//static global database
+$globalConfig['dataSources']['globalDB']['hostname'] = 'localhost';
+$globalConfig['dataSources']['globalDB']['username'] = 'root';
+$globalConfig['dataSources']['globalDB']['password'] = '';
+$globalConfig['dataSources']['globalDB']['database'] = 'main';
+$globalConfig['dataSources']['globalDB']['charset'] = 'utf8mb4';
+
+
